646bf3e8ff1306851eae5d7ee729eba599d0cebb
mastergun <martimendrugu@gmail.com>
2018-06-20 18:54:55 +0200

    Initial commit

---
+++ Source/OnlinePrototype/OnlinePrototypeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OnlinePrototypeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOnlinePrototypeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOnlinePrototypeGameMode();
+};
+
+
+
